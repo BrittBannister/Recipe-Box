3855566c653d2c9518d58829eaae800ace073df9
Britt <brittbannister1127@icloud.com>
2021-02-03 13:38:28 -0500

    created author and recipe models, migrated them and pushed to database

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+'''
+AUTHOR:
+name = charfield
+bio = textfield
 
-# Create your models here.
+RECIPE:
+title = charfield
+author = foreign key
+description = textfield
+time required = charfield (ex: one hour)
+instructions = textfield
+'''
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=150)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    time_required = models.CharField(max_length=100)
+    instructions = models.TextField()
