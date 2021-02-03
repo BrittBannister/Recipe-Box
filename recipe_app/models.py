b8a8fa26d7b828bc21abb195cda48408e9d2c758
Britt <brittbannister1127@icloud.com>
2021-02-03 14:09:13 -0500

    registered models and they are showing up on the admin page!

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -16,9 +16,15 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     description = models.TextField()
     time_required = models.CharField(max_length=100)
     instructions = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} | {self.author}"
