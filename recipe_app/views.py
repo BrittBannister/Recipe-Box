590ea6431d70b2497c1399c361dd85f72b8f442a
Britt <brittbannister1127@icloud.com>
2021-02-03 14:29:24 -0500

    added the recipes to the main page view and they are now displaying added recipes and authors to the screen!

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from recipe_app.models import Author, Recipe
+
 def index_view(requests):
-    return render(requests, 'index.html', {'heading':'Recipe Box.'})
+    recipes = Recipe.objects.all()
+    return render(requests, 'index.html', {'heading':'Recipe Box.', 'recipes': recipes})
