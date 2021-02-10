e0fadf752232bf402a1423f779aba6394f72c479
Britt <brittbannister1127@icloud.com>
2021-02-10 00:07:05 -0500

    changed add author form from model form to a simple form and added username and password fields

--- recipe_app/forms.py
+++ recipe_app/forms.py
@@ -8,10 +8,26 @@ class AddRecipeForm(forms.Form):
     time_required = forms.CharField(max_length=100)
     instructions = forms.CharField(widget=forms.Textarea)
 
-class AddAuthorForm(forms.ModelForm):
-    class Meta:
-        model = Author
-        fields = [
-            'name',
-            'bio',
-        ]
+# class AddAuthorForm(forms.ModelForm):
+#     class Meta:
+#         model = Author
+#         fields = [
+#             'name',
+#             'bio',
+#         ]
+
+class AddAuthorForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    bio = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=40)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class SignUpForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    bio = forms.CharField(widget=forms.Textarea)
+    username = forms.CharField(max_length=40)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=40)
+    password = forms.CharField(widget=forms.PasswordInput) 
